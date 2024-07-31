d34b9137796d81f05ebd7230e6fc15bff989b21b
Shrusti Vaidya <shrustivaidya2001@gmail.com>
2024-07-31 22:41:18 +0530

    Checkout Process

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import { Form, Button, Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
+import { savePaymentMethod } from '../slices/cartSlice';
+
+const PaymentScreen = () => {
+  const navigate = useNavigate();
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      navigate('/shipping');
+    }
+  }, [navigate, shippingAddress]);
+
+  const [paymentMethod, setPaymentMethod] = useState('PayPal');
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate('/placeorder');
+  };
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label as='legend'>Select Method</Form.Label>
+          <Col>
+            <Form.Check
+              className='my-2'
+              type='radio'
+              label='PayPal or Credit Card'
+              id='PayPal'
+              name='paymentMethod'
+              value='PayPal'
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+          </Col>
+        </Form.Group>
+
+        <Form.Group className='my-2' controlId='submit'>
+        <Button type='submit' variant='primary' className='w-100'>
+          Continue
+        </Button>
+        </Form.Group>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default PaymentScreen;
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { updateCart } from '../utils/cartUtils';
 
 const initialState = localStorage.getItem("cart")
-? JSON.parse(localStorage.getItem('cart')) : { cartItems: []};
+? JSON.parse(localStorage.getItem('cart')) : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
 
 
 const cartSlice = createSlice({
@@ -28,11 +28,23 @@ const cartSlice = createSlice({
           state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
           return updateCart(state);
         },
+        saveShippingAddress: (state, action) => {
+          state.shippingAddress = action.payload;
+          return updateCart(state);
+        },
+        savePaymentMethod: (state, action) => {
+          state.paymentMethod = action.payload;
+          localStorage.setItem('cart', JSON.stringify(state));
+        },
+        clearCartItems: (state, action) => {
+          state.cartItems = [];
+          localStorage.setItem('cart', JSON.stringify(state));
+        },
     },
 });
 
 
 export const {addToCart,
-  removeFromCart,} = cartSlice.actions;
+  removeFromCart,saveShippingAddress,savePaymentMethod,clearCartItems} = cartSlice.actions;
 
 export default cartSlice.reducer;
