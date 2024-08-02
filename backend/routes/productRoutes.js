584d0e245cc364055906199648663ae4daf84167
Shrusti Vaidya <shrustivaidya2001@gmail.com>
2024-08-02 11:39:16 +0530

    Review and Comment On the Product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { getProducts,getProductById, createProduct, updateProduct,deleteProduct, } from "../controllers/productControllers.js";
+import { getProducts,getProductById, createProduct, updateProduct,deleteProduct,createProductReview, } from "../controllers/productControllers.js";
 import { protect, admin } from '../middleware/authmiddleware.js';
 
 router.route('/').get(getProducts).post(protect,admin,createProduct);
 router.route('/:id').get(getProductById).put(protect,admin,updateProduct).delete(protect,admin,deleteProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 
 
